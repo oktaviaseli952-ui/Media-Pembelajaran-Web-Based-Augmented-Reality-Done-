2b67b19fae219ecfb1c40bc747e27b3a7e6f8785
oktaviaseli952-ui <oktaviaseli952@gmail.com>
2026-05-11 10:14:47 +0700

    Media pembelajaran web-based augmented reality

---
+++ story_content/user.js
@@ -0,0 +1,158 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6jQj5Pi4aUE":
+        Script1();
+        break;
+      case "5ux2rFofnbu":
+        Script2();
+        break;
+      case "6ppMVL0Q0mk":
+        Script3();
+        break;
+      case "6hkOnwNigOl":
+        Script4();
+        break;
+      case "6nEElEa1KN8":
+        Script5();
+        break;
+      case "6cZg3DHekBv":
+        Script6();
+        break;
+      case "6PMzNrTaVbN":
+        Script7();
+        break;
+      case "6ppKTxsY8PL":
+        Script8();
+        break;
+      case "5yj5Ew3NtuI":
+        Script9();
+        break;
+      case "5d3Ig7nHELv":
+        Script10();
+        break;
+      case "6VwDc1Rzyfs":
+        Script11();
+        break;
+      case "6Ajj9owUjK1":
+        Script12();
+        break;
+      case "5lIYIlasxcN":
+        Script13();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.src="musik.mp3";
+
+audio.load();
+
+audio.play();
+audio.volume=0,3;
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSongku');
+audio.src="musik.mp3";
+
+audio.load();
+
+audio.play();
+
+
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSong');
+
+
+audio.volume = 0.0;
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSong');
+
+
+audio.volume = 0.1;
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSong');
+
+
+audio.volume = 0.2;
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSong');
+
+
+audio.volume = 0.3;
+}
+
+function Script7()
+{
+  var audio = document.getElementById('bgSong');
+
+
+audio.volume = 0.4;
+}
+
+function Script8()
+{
+  var audio = document.getElementById('bgSong');
+
+
+audio.volume = 0.5;
+}
+
+function Script9()
+{
+  var audio = document.getElementById('bgSong');
+
+
+audio.volume = 0.6;
+}
+
+function Script10()
+{
+  var audio = document.getElementById('bgSong');
+
+
+audio.volume = 0.7;
+}
+
+function Script11()
+{
+  var audio = document.getElementById('bgSong');
+
+
+audio.volume = 0.8;
+}
+
+function Script12()
+{
+  var audio = document.getElementById('bgSong');
+
+
+audio.volume = 0.9;
+}
+
+function Script13()
+{
+  var audio = document.getElementById('bgSong');
+
+
+audio.volume = 1.0;
+}
+
